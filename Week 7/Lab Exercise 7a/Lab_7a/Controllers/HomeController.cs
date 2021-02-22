fbf3ee16219fc46425016c70f0bacf0077d45706
Resume Website <jdcollins242@gmail.com>
2021-02-22 13:20:28 -0500

    Completed Midterm

---
+++ Week 7/Lab Exercise 7a/Lab_7a/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_7a.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Create a new pet object
+            Models.Cat myCat = new Models.Cat()
+            {
+                Name = "White Legs",
+                Type = "Savannah",
+                DOB = new DateTime(2017, 07, 04)
+            };
+
+            return View(myCat);
+        }
+    }
+}
---
+++ Week 7/Lab Exercise 7a/Lab_7a/Models/Cat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_7a.Models
+{
+    public class Cat
+    {
+        // Properties
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public DateTime DOB { get; set; }
+    }
+}
