cca6191c738bf053cb9b0748177eb1a5153530cb
Resume Website <jdcollins242@gmail.com>
2021-02-26 09:04:50 -0500

    Assignment 3 and Lab 7b Completed

---
+++ Week 7/Assignment3/Assignment 3/Models/Coin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_7b.Models
+{
+    public class Coin
+    {
+        public int ScoreOne { get; set; }
+        public int ScoreTwo { get; set; }
+
+    }
+}
---
+++ Week 7/Lab Exercise 7b/Lab_7b/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_7b.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Models.Coin MainCoin = new Models.Coin()
+            {
+                ScoreOne = 0,
+                ScoreTwo = 0
+            };
+            //return View();
+            return View(MainCoin);
+        }
+    }
+}
